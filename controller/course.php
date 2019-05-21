faa15eafe54da8063cdf86a01ee6a7a3bcf58723
harisliv <livieratos.theoxaris@gmail.com>
2019-05-21 13:04:29 +0300

    new course api

--- controller/course.php
+++ controller/course.php
@@ -306,31 +306,43 @@ elseif(empty($_GET)) {
       }
 
       // check if post request contains title and completed data in body as these are mandatory
-      if(!isset($jsonData->title) || !isset($jsonData->completed)) {
+      if(!isset($jsonData->id) || !isset($jsonData->name) || !isset($jsonData->curr) || !isset($jsonData->hours_theory) || !isset($jsonData->hours_lab) || !isset($jsonData->hours_practice)) {
         $response = new Response();
         $response->setHttpStatusCode(400);
         $response->setSuccess(false);
-        (!isset($jsonData->title) ? $response->addMessage("Title field is mandatory and must be provided") : false);
-        (!isset($jsonData->completed) ? $response->addMessage("Completed field is mandatory and must be provided") : false);
+        (!isset($jsonData->title) ? $response->addMessage("ID field is mandatory and must be provided") : false);
+        (!isset($jsonData->name) ? $response->addMessage("Name field is mandatory and must be provided") : false);
+        (!isset($jsonData->curr) ? $response->addMessage("Programma spoudwn field is mandatory and must be provided") : false);
+        (!isset($jsonData->hours_theory) ? $response->addMessage("hours_theory field is mandatory and must be provided") : false);
+        (!isset($jsonData->hours_lab) ? $response->addMessage("hours_lab field is mandatory and must be provided") : false);
+        (!isset($jsonData->hours_practice) ? $response->addMessage("hours_practice field is mandatory and must be provided") : false);
         $response->send();
         exit;
       }
 
       // create new task with data, if non mandatory fields not provided then set to null
-      $newTask = new Task(null, $jsonData->title, (isset($jsonData->description) ? $jsonData->description : null), (isset($jsonData->deadline) ? $jsonData->deadline : null), $jsonData->completed);
+      $newCourse = new Course($jsonData->id, $jsonData->name, $jsonData->curr, (isset($jsonData->period) ? $jsonData->period : "-"), (isset($jsonData->active) ? $jsonData->active : "Y"), $jsonData->hours_theory, $jsonData->hours_lab, $jsonData->hours_practice);
       // get title, description, deadline, completed and store them in variables
-      $title = $newTask->getTitle();
-      $description = $newTask->getDescription();
-      $deadline = $newTask->getDeadline();
-      $completed = $newTask->getCompleted();
+      $id = $newCourse->getID();
+      $name = $newCourse->getName();
+      $curr = $newCourse->getCurr();
+      $period = $newCourse->getPeriod();
+      $active = $newCourse->getActive();
+      $hours_theory = $newCourse->getHoursTheory();
+      $hours_lab = $newCourse->getHoursLab();
+      $hours_practice = $newCourse->getHoursPractice();
 
       // ADD AUTH TO QUERY
       // create db query
-      $query = $writeDB->prepare('insert into tbltasks (title, description, deadline, completed, userid) values (:title, :description, STR_TO_DATE(:deadline, \'%d/%m/%Y %H:%i\'), :completed, :userid)');
-      $query->bindParam(':title', $title, PDO::PARAM_STR);
-      $query->bindParam(':description', $description, PDO::PARAM_STR);
-      $query->bindParam(':deadline', $deadline, PDO::PARAM_STR);
-      $query->bindParam(':completed', $completed, PDO::PARAM_STR);
+      $query = $writeDB->prepare('insert into course_list (id, name, curr, period, active, hours_theory, hours_lab, hours_practice, userid) values (:id, :name, :curr, :period, :active, :hours_theory, :hours_lab, :hours_practice, :userid)');
+      $query->bindParam(':id', $id, PDO::PARAM_STR);
+      $query->bindParam(':name', $name, PDO::PARAM_STR);
+      $query->bindParam(':curr', $curr, PDO::PARAM_STR);
+      $query->bindParam(':period', $period, PDO::PARAM_STR);
+      $query->bindParam(':active', $active, PDO::PARAM_STR);
+      $query->bindParam(':hours_theory', $hours_theory, PDO::PARAM_INT);
+      $query->bindParam(':hours_lab', $hours_lab, PDO::PARAM_INT);
+      $query->bindParam(':hours_practice', $hours_practice, PDO::PARAM_INT);
       $query->bindParam(':userid', $returned_userid, PDO::PARAM_INT);
       $query->execute();
 
@@ -349,11 +361,11 @@ elseif(empty($_GET)) {
       }
 
       // get last task id so we can return the Task in the json
-      $lastTaskID = $writeDB->lastInsertId();
+      $lastCourseID = $writeDB->lastInsertId();
       // ADD AUTH TO QUERY
       // create db query to get newly created task - get from master db not read slave as replication may be too slow for successful read
-      $query = $writeDB->prepare('SELECT id, title, description, DATE_FORMAT(deadline, "%d/%m/%Y %H:%i") as deadline, completed from tbltasks where id = :taskid and userid = :userid');
-      $query->bindParam(':taskid', $lastTaskID, PDO::PARAM_INT);
+      $query = $writeDB->prepare('SELECT id, name, curr, period, active, hours_theory, hours_lab, hours_practice from course_list where id = :id and userid = :userid');
+      $query->bindParam(':id', $id, PDO::PARAM_STR);
       $query->bindParam(':userid', $returned_userid, PDO::PARAM_INT);
       $query->execute();
 
@@ -372,26 +384,26 @@ elseif(empty($_GET)) {
       }
 
       // create empty array to store tasks
-      $taskArray = array();
+      $courseArray = array();
 
       // for each row returned - should be just one
       while($row = $query->fetch(PDO::FETCH_ASSOC)) {
         // create new task object
-        $task = new Task($row['id'], $row['title'], $row['description'], $row['deadline'], $row['completed']);
+        $course = new Course($row['id'], $row['name'], $row['curr'], $row['period'], $row['active'], $row['hours_theory'], $row['hours_lab'], $row['hours_practice']);
 
         // create task and store in array for return in json data
-        $taskArray[] = $task->returnTaskAsArray();
+        $courseArray[] = $course->returnCourseAsArray();
       }
       // bundle tasks and rows returned into an array to return in the json data
       $returnData = array();
       $returnData['rows_returned'] = $rowCount;
-      $returnData['tasks'] = $taskArray;
+      $returnData['courses'] = $courseArray;
 
       //set up response for successful return
       $response = new Response();
       $response->setHttpStatusCode(201);
       $response->setSuccess(true);
-      $response->addMessage("Task created");
+      $response->addMessage("Course created");
       $response->setData($returnData);
       $response->send();
       exit;
@@ -411,7 +423,7 @@ elseif(empty($_GET)) {
       $response = new Response();
       $response->setHttpStatusCode(500);
       $response->setSuccess(false);
-      $response->addMessage("Failed to insert task into database - check submitted data for errors");
+      $response->addMessage("Failed to insert course into database - check submitted data for errors");
       $response->send();
       exit;
     }
