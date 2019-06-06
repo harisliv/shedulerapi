0103f8c3432dc2f872cd77f609bf2f8df04052ec
harisliv <livieratos.theoxaris@gmail.com>
2019-06-06 15:02:12 +0300

    simple room api

---
+++ controller/room.php
@@ -0,0 +1,187 @@
+  <?php
+
+  //header('Content-Type: text/html; charset=utf-8');
+  require_once('db.php');
+  require_once('../model/room.php');
+  require_once('../model/response.php');
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+  //error_reporting(0);
+
+  // attempt to set up connections to read and write db connections
+  try {
+    $writeDB = DB::connectWriteDB();
+    $readDB = DB::connectReadDB();
+  }
+  catch(PDOException $ex) {
+    // log connection error for troubleshooting and return a json error response
+    error_log("Connection Error: ".$ex, 0);
+    $response = new Response();
+    $response->setHttpStatusCode(500);
+    $response->setSuccess(false);
+    $response->addMessage("Database connection error");
+    $response->send();
+    exit;
+  }
+
+  // BEGIN OF AUTH SCRIPT
+  // Authenticate user with access token
+  // check to see if access token is provided in the HTTP Authorization header and that the value is longer than 0 chars
+  // don't forget the Apache fix in .htaccess file
+  if(!isset($_SERVER['HTTP_AUTHORIZATION']) || strlen($_SERVER['HTTP_AUTHORIZATION']) < 1)
+  {
+    $response = new Response();
+    $response->setHttpStatusCode(401);
+    $response->setSuccess(false);
+    (!isset($_SERVER['HTTP_AUTHORIZATION']) ? $response->addMessage("Access token is missing from the header") : false);
+    (strlen($_SERVER['HTTP_AUTHORIZATION']) < 1 ? $response->addMessage("Access token cannot be blank") : false);
+    $response->send();
+    exit;
+  }
+
+  // get supplied access token from authorisation header - used for delete (log out) and patch (refresh)
+  $accesstoken = $_SERVER['HTTP_AUTHORIZATION'];
+
+  // attempt to query the database to check token details - use write connection as it needs to be synchronous for token
+  try {
+    // create db query to check access token is equal to the one provided
+    $query = $writeDB->prepare('select userid, accesstokenexpiry, useractive, loginattempts from tblsessions, tblusers where tblsessions.userid = tblusers.id and accesstoken = :accesstoken');
+    $query->bindParam(':accesstoken', $accesstoken, PDO::PARAM_STR);
+    $query->execute();
+
+    // get row count
+    $rowCount = $query->rowCount();
+
+    if($rowCount === 0) {
+      // set up response for unsuccessful log out response
+      $response = new Response();
+      $response->setHttpStatusCode(401);
+      $response->setSuccess(false);
+      $response->addMessage("Invalid access token");
+      $response->send();
+      exit;
+    }
+
+    // get returned row
+    $row = $query->fetch(PDO::FETCH_ASSOC);
+
+    // save returned details into variables
+    $returned_accesstokenexpiry = $row['accesstokenexpiry'];
+    $returned_useractive = $row['useractive'];
+    $returned_loginattempts = $row['loginattempts'];
+
+    // check if account is active
+    if($returned_useractive != 'Y') {
+      $response = new Response();
+      $response->setHttpStatusCode(401);
+      $response->setSuccess(false);
+      $response->addMessage("User account is not active");
+      $response->send();
+      exit;
+    }
+
+    // check if account is locked out
+    if($returned_loginattempts >= 3) {
+      $response = new Response();
+      $response->setHttpStatusCode(401);
+      $response->setSuccess(false);
+      $response->addMessage("User account is currently locked out");
+      $response->send();
+      exit;
+    }
+
+    // check if access token has expired
+    if(strtotime($returned_accesstokenexpiry) < time()) {
+      $response = new Response();
+      $response->setHttpStatusCode(401);
+      $response->setSuccess(false);
+      $response->addMessage("Access token has expired");
+      $response->send();
+      exit;
+    }
+
+    if(empty($_GET)) {
+
+      // if request is a GET e.g. get rooms
+      if($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+        // attempt to query the database
+        try {
+          // ADD AUTH TO QUERY
+          // create db query
+          $query = $readDB->prepare('SELECT id, lektiko_room, room_code from room');
+          $query->execute();
+
+          // get row count
+          $rowCount = $query->rowCount();
+
+          // create room array to store returned rooms
+          $roomArray = array();
+
+          // for each row returned
+          while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+            // create new room object for each row
+            echo "<br>" . $row['lektiko_room'];
+            $room = new Room($row['id'], $row['lektiko_room'], $row['room_code']);
+
+            // create room and store in array for return in json data
+            $roomArray[] = $room->returnRoomAsArray();
+          }
+
+          // bundle rooms and rows returned into an array to return in the json data
+          $returnData = array();
+          $returnData['rows_returned'] = $rowCount;
+          $returnData['rooms'] = $roomArray;
+
+          // set up response for successful return
+          $response = new Response();
+          $response->setHttpStatusCode(200);
+          $response->setSuccess(true);
+          $response->toCache(true);
+          $response->setData($returnData);
+          $response->send();
+          exit;
+        }
+        // if error with sql query return a json error
+        catch(RoomException $ex) {
+          $response = new Response();
+          $response->setHttpStatusCode(500);
+          $response->setSuccess(false);
+          $response->addMessage($ex->getMessage());
+          $response->send();
+          exit;
+        }
+        catch(PDOException $ex) {
+          error_log("Database Query Error: ".$ex, 0);
+          $response = new Response();
+          $response->setHttpStatusCode(500);
+          $response->setSuccess(false);
+          $response->addMessage("Failed to get rooms");
+          $response->send();
+          exit;
+        }
+      }
+
+
+    }
+
+    else {
+      $response = new Response();
+      $response->setHttpStatusCode(404);
+      $response->setSuccess(false);
+      $response->addMessage("Endpoint not found");
+      $response->send();
+      exit;
+    }
+
+  }
+
+    catch(PDOException $ex) {
+      $response = new Response();
+      $response->setHttpStatusCode(500);
+      $response->setSuccess(false);
+      $response->addMessage("There was an issue authenticating - please try again");
+      $response->send();
+      exit;
+    }
---
+++ model/Room.php
@@ -0,0 +1,61 @@
+  <?php
+
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+  //error_reporting(0);
+
+    class RoomException extends Exception { }
+
+    class Room {
+
+
+      private $_id;
+      private $_lektiko_room;
+      private $_room_code;
+
+      public function __construct($id, $lektiko_room , $room_code){
+        $this->setID($id);
+        $this->setLektikoRoom($lektiko_room);
+        $this->setRoomCode($room_code);
+        }
+
+      public function getID() {return $this->_id;}
+      public function getLektikoRoom() {return $this->_lektiko_room;}
+      public function getRoomCode() {return $this->_room_code;}
+
+      public function setID($id) {
+        if(($id !== null) && (!is_numeric($id) || $id < 0)){
+          throw new RoomException("Room availability ID error");
+        }
+          $this->_id = $id;
+      }
+
+      public function setLektikoRoom($lektiko_room) {
+        if(strlen($lektiko_room) < 0 || strlen($lektiko_room) > 255){
+        {
+          throw new RoomException("Room ID error");
+        }
+          $this->_lektiko_room = $lektiko_room;
+      }
+    }
+
+      public function setRoomCode($room_code) {
+        if(strlen($room_code) < 0 || strlen($room_code) > 150){
+          throw new RoomException("Time Slot ID error");
+        }
+          $this->_room_code = $room_code;
+      }
+
+
+      public function returnRoomAsArray() {
+        $room = array();
+        $room['id'] = $this->getID();
+        $room['lektiko_room'] = $this->getLektikoRoom();
+        $room['room_code'] = $this->getRoomCode();
+
+        return $room;
+      }
+
+
+    }
