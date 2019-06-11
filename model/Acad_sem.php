f9683674bc5e59fd8b2f00eb43697640eeb1fe7b
harisliv <livieratos.theoxaris@gmail.com>
2019-06-11 16:49:18 +0300

    course this year api

--- model/Acad_sem.php
+++ model/Acad_sem.php
@@ -12,20 +12,20 @@
 
       private $_id;
       private $_acad_year;
-      private $_lektiko_acadsem;
       private $_type_sem;
+      private $_lektiko_acadsem;
 
-      public function __construct($id, $acad_year, $lektiko_acadsem , $type_sem){
+      public function __construct($id, $acad_year,  $type_sem, $lektiko_acadsem){
         $this->setID($id);
         $this->setAcadYear($acad_year);
-        $this->setLektikoAcadsem($lektiko_acadsem);
         $this->setTypeSem($type_sem);
+        $this->setLektikoAcadsem($lektiko_acadsem);
         }
 
       public function getID() {return $this->_id;}
       public function getAcadYear() {return $this->_acad_year;}
-      public function getLektikoAcadsem() {return $this->_lektiko_acadsem;}
       public function getTypeSem() {return $this->_type_sem;}
+      public function getLektikoAcadsem() {return $this->_lektiko_acadsem;}
 
       public function setID($id) {
         if(($id !== null) && (!is_numeric($id) || $id < 0)){
@@ -41,6 +41,13 @@
           $this->_acad_year = $acad_year;
       }
 
+        public function setTypeSem($type_sem) {
+          if(strtoupper($type_sem) !== 'W' && strtoupper($type_sem) !== 'S'){
+            throw new AcadSemException("type_sem must by w or s");
+          }
+            $this->_type_sem = $type_sem;
+        }
+
       public function setLektikoAcadsem($lektiko_acadsem) {
         if(strlen($lektiko_acadsem) < 0 || strlen($lektiko_acadsem) > 255){
           throw new AcadSemException("_lektiko_acadsem error");
@@ -48,20 +55,13 @@
         $this->_lektiko_acadsem = $lektiko_acadsem;
     }
 
-      public function setTypeSem($type_sem) {
-        if($type_sem !== 'w' && $type_sem !== 's'){
-          throw new AcadSemException("type_sem must by w or s");
-        }
-          $this->_type_sem = $type_sem;
-      }
-
 
       public function returnAcadSemAsArray() {
         $acadsem = array();
         $acadsem['id'] = $this->getID();
         $acadsem['acad_year'] = $this->getAcadYear();
-        $acadsem['lektiko_acadsem'] = $this->getLektikoAcadsem();
         $acadsem['type_sem'] = $this->getTypeSem();
+        $acadsem['lektiko_acadsem'] = $this->getLektikoAcadsem();
 
         return $acadsem;
       }
---
+++ model/Course_this_year.php
@@ -0,0 +1,104 @@
+<?php
+
+
+//header('Content-Type: text/html; charset=utf-8');
+
+
+  class Course_this_yearException extends Exception { }
+
+  class Course_this_year {
+
+
+    private $_id;
+    private $_id_course;
+    private $_id_responsible_prof;
+    private $_id_acadsem;
+    private $_count_div_theory;
+    private $_count_div_lab;
+    private $_count_div_practice;
+    //private $_userid;
+
+    public function __construct($id, $id_course, $id_responsible_prof, $id_acadsem, $count_div_theory , $count_div_lab , $count_div_practice){
+      $this->setID($id);
+      $this->setIdCourse($id_course);
+      $this->setIdResponsibleProf($id_responsible_prof);
+      $this->setIdAcadsem($id_acadsem);
+      $this->setCountDivTheory($count_div_theory);
+      $this->setCountDivLab($count_div_lab);
+      $this->setCountDivPractice($count_div_practice);
+      }
+
+    public function getID() {return $this->_id;}
+    public function getIdCourse() {return $this->_id_course;}
+    public function getIdResponsibleProf() {return $this->_id_responsible_prof;}
+    public function getIdAcadsem() {return $this->_id_acadsem;}
+    public function getCountDivTheory() {return $this->_count_div_theory;}
+    public function getCountDivLab() {return $this->_count_div_lab;}
+    public function getCountDivPractice() {return $this->_count_div_practice;}
+
+    public function setID($id) {
+      if(($id !== null) && (!is_numeric($id) || $id < 0)){
+        throw new Course_this_yearException("AcadSem availability ID error");
+      }
+        $this->_id = $id;
+    }
+
+    public function setIdCourse($id_course) {
+      if(strlen($id_course) < 0 || strlen($id_course) > 150){
+        throw new Course_this_yearException("Course_this_year id_course error");
+      }
+      $this->_id_course=$id_course;
+    }
+
+    public function setIdResponsibleProf($id_responsible_prof) {
+      if(strlen($id_responsible_prof) < 0 || strlen($id_responsible_prof) > 150){
+        throw new Course_this_yearException("id_responsible_prof Programma Spoudwn error");
+      }
+      $this->_id_responsible_prof=$id_responsible_prof;
+    }
+
+    public function setIdAcadsem($id_acadsem) {
+      if(($id_acadsem !== null) && (!is_numeric($id_acadsem) || $id_acadsem < 0)){
+        throw new Course_this_yearException("Course_this_year Programma Spoudwn error");
+      }
+      $this->_id_acadsem=$id_acadsem;
+    }
+
+    public function setCountDivTheory($count_div_theory) {
+      if(($count_div_theory !== null) && (!is_numeric($count_div_theory) || $count_div_theory < 0 || $count_div_theory > 9 ))
+      {
+        throw new Course_this_yearException("HOURS THEORY error");
+      }
+        $this->_count_div_theory = $count_div_theory;
+    }
+
+    public function setCountDivLab($count_div_lab) {
+      if(($count_div_lab !== null) && (!is_numeric($count_div_lab) || $count_div_lab < 0 || $count_div_lab > 9)){
+        throw new Course_this_yearException("Course_this_year ID error");
+      }
+        $this->_count_div_lab = $count_div_lab;
+    }
+
+    public function setCountDivPractice($count_div_practice) {
+      if(($count_div_practice !== null) && (!is_numeric($count_div_practice) || $count_div_practice < 0 || $count_div_practice > 9)){
+        throw new Course_this_yearException("Course_this_year ID error");
+      }
+        $this->_count_div_practice = $count_div_practice;
+    }
+
+
+    public function returnCourse_this_yearAsArray() {
+      $coursethisyear = array();
+      $coursethisyear['id'] = $this->getID();
+      $coursethisyear['id_course'] = $this->getIdCourse();
+      $coursethisyear['id_responsible_prof'] = $this->getIdResponsibleProf();
+      $coursethisyear['id_acadsem'] = $this->getIdAcadsem();
+      $coursethisyear['count_div_theory'] = $this->getCountDivTheory();
+      $coursethisyear['count_div_lab'] = $this->getCountDivLab();
+      $coursethisyear['count_div_practice'] = $this->getCountDivPractice();
+
+      return $coursethisyear;
+    }
+
+
+  }
